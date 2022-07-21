013291d895e0867099d810f5d962f3dced3195c6
Ambareen09 <ambubushra@gmail.com>
2022-07-21 20:07:33 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,9 @@
 
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({
+  baseURL: 'https://create-memories-daily.herokuapp.com/',
+})
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
