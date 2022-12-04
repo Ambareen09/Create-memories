802bfc033b2eb2a78e4f0b57ad718e3cc8af352e
Ambareen09 <ambubushra@gmail.com>
2022-12-04 18:01:41 +0530

    bug fixes

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,45 +1,66 @@
-/** @format */
-
 import {
+  START_LOADING,
+  END_LOADING,
   FETCH_ALL,
+  FETCH_POST,
   FETCH_BY_SEARCH,
   CREATE,
   UPDATE,
   DELETE,
   LIKE,
 } from '../constants/actionTypes'
-
 import * as api from '../api/index.js'
+
+export const getPost = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING })
+
+    const { data } = await api.fetchPost(id)
+
+    dispatch({ type: FETCH_POST, payload: { post: data } })
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 export const getPosts = (page) => async (dispatch) => {
   try {
-    const { data } = await api.fetchPosts(page)
-    console.log(data)
+    dispatch({ type: START_LOADING })
+    const {
+      data: { data, currentPage, numberOfPages },
+    } = await api.fetchPosts(page)
 
-    dispatch({ type: FETCH_ALL, payload: data })
+    dispatch({ type: FETCH_ALL, payload: { data, currentPage, numberOfPages } })
+    dispatch({ type: END_LOADING })
   } catch (error) {
-    console.log(error.message)
+    console.log(error)
   }
 }
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING })
     const {
       data: { data },
     } = await api.fetchPostsBySearch(searchQuery)
-    dispatch({ type: FETCH_BY_SEARCH, payload: data })
-  } catch (e) {
-    console.log(e)
+
+    dispatch({ type: FETCH_BY_SEARCH, payload: { data } })
+    dispatch({ type: END_LOADING })
+  } catch (error) {
+    console.log(error)
   }
 }
 
-export const createPost = (post) => async (dispatch) => {
+export const createPost = (post, navigate) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING })
     const { data } = await api.createPost(post)
 
     dispatch({ type: CREATE, payload: data })
+
+    navigate(`/posts/${data._id}`)
   } catch (error) {
-    console.log(error.message)
+    console.log(error)
   }
 }
 
@@ -49,13 +70,15 @@ export const updatePost = (id, post) => async (dispatch) => {
 
     dispatch({ type: UPDATE, payload: data })
   } catch (error) {
-    console.log(error.message)
+    console.log(error)
   }
 }
 
 export const likePost = (id) => async (dispatch) => {
+  const user = JSON.parse(localStorage.getItem('profile'))
+
   try {
-    const { data } = await api.likePost(id)
+    const { data } = await api.likePost(id, user?.token)
 
     dispatch({ type: LIKE, payload: data })
   } catch (error) {
@@ -65,10 +88,10 @@ export const likePost = (id) => async (dispatch) => {
 
 export const deletePost = (id) => async (dispatch) => {
   try {
-    await api.deletePost(id)
+    await await api.deletePost(id)
 
     dispatch({ type: DELETE, payload: id })
   } catch (error) {
-    console.log(error.message)
+    console.log(error)
   }
 }
